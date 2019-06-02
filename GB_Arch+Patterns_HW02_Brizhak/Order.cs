431cf7779448579ae5095f2d26ac50d08cacb07d
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-02 11:27:38 +0800

    + class Order

---
+++ GB_Arch+Patterns_HW02_Brizhak/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_HW02_Brizhak
+{
+    class Order
+    {
+        private int orderId;
+        private string Name;
+    }
+}
