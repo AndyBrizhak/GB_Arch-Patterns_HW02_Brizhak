cc9d5738d6efa18e784a3fd682f0dba1fb5161fd
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-02 12:32:32 +0800

    +interface Cars

---
+++ HW02Task02Brizhak/Cars.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW02Task02Brizhak
+{
+    class Cars : IItem
+    {
+        public Cars()
+        {
+        }
+
+        public void SetDiscount(double discount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPromocode(string promocode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPrice(double price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetState(State state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
