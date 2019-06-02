50d8155cb20398f96252ab1f415212b838c3ff47
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-02 12:02:09 +0800

    +State

---
+++ HW02Task02Brizhak/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW02Task02Brizhak
+{
+    class State
+    {
+    }
+}
