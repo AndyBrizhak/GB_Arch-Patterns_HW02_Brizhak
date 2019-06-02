9bda818cbacd9ba5b0e7ee0e7507b9bfc5158c8d
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-02 12:28:59 +0800

    + interface Book

--- HW02Task02Brizhak/Book.cs
+++ HW02Task02Brizhak/Book.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace HW02Task02Brizhak
 {
-    class Book
+    class Book : IItem
     {
+        public Book()
+        {
+        }
+
+        public void SetDiscount(double discount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPrice(double price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPromocode(string promocode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetState(State state)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
