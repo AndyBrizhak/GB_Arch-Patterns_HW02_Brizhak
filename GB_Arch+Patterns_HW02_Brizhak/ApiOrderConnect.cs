cb97c20c316c787ba9528be2d46c0cb7e59d035e
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-02 17:49:56 +0800

    change to ApiOrderConnect

---
+++ GB_Arch+Patterns_HW02_Brizhak/ApiOrderConnect.cs
@@ -0,0 +1,13 @@
+﻿namespace GB_Arch_Patterns_HW02_Brizhak
+{
+    class ApiOrderConnect : IOrderConnect
+    {
+        public new Order Load(int orderId)
+        {
+            return null;
+        }
+        public  void Save(Order order) { }
+        public  void Update(Order order) {  }
+        public  void Delete(Order order) {  }
+    }
+}
