d7c85ea3a3318b60e8dd783e1315c505ea6a4c49
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-02 12:26:08 +0800

    + interfaces Cloth

--- HW02Task02Brizhak/Clothes.cs
+++ HW02Task02Brizhak/Clothes.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace HW02Task02Brizhak
 {
-    class Clothes
+    class Clothes : IItem, IMaterial, IColor, ISize
     {
+        public Clothes()
+        {
+        }
+
+        public void SetColor(Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetDiscount(double discount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMaterial(Material material)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPrice(double price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPromocode(string promocode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSize(Size size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetState(State state)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
