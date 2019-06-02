1799d73ada33149a7af5a274fe80a5e50931b819
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-02 11:32:47 +0800

    +class OrderRepository

---
+++ GB_Arch+Patterns_HW02_Brizhak/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_HW02_Brizhak
+{
+    class OrderRepository
+    {
+        public Order Load(int orderId)
+        {
+            return null;
+        }
+
+        public void Save(Order order)
+        {  }
+
+        public void Update(Order order)
+        { }
+
+        public void Delete(Order order)
+        { }
+    }
+}
