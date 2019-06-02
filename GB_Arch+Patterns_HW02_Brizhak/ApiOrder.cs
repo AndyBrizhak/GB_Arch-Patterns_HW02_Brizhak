f3cff504f3d0c17c852563fdfda85e9913af86a8
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-02 11:44:35 +0800

    + class ApiOrder

---
+++ GB_Arch+Patterns_HW02_Brizhak/ApiOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_HW02_Brizhak
+{
+    class ApiOrder : OrderRepository
+    {
+        public new Order Load(int orderId)
+        {
+            return null;
+
+        }
+        public new void Save(Order order) { }
+        public new void Update(Order order) {  }
+        public new void Delete(Order order) {  }
+    }
+}
